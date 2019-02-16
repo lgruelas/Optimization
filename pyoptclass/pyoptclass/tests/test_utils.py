06b7fc89fed4025de0e1eec04ff5ae84a539c95a
Germán Ruelas <lgruelas@hotmail.com>
2019-02-16 15:25:24 -0600

    test

--- pyoptclass/pyoptclass/tests/test_utils.py
+++ pyoptclass/pyoptclass/tests/test_utils.py
@@ -5,8 +5,8 @@ import random
 def test_sortByX():
     a = [classes.Point2D(i, 1-i) for i in xrange(10)]
     random.shuffle(a)
-    utils.sortByX(a)
-    assert a == [classes.Point2D(i, 1-i) for i in xrange(10)]
+    utils.sortByY(a)
+    assert a == [classes.Point2D(i, 1-i) for i in xrange(9, -1, -1)]
 
 def test_sortByY():
     a = [classes.Point2D(i, 1-i) for i in xrange(10)]
