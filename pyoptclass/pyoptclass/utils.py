8c55b607d3b7b12671d0c8090817e50420dac8e3
Germán Ruelas <lgruelas@hotmail.com>
2019-02-18 19:09:39 -0600

    Migrate do codecov

--- pyoptclass/pyoptclass/utils.py
+++ pyoptclass/pyoptclass/utils.py
@@ -22,7 +22,7 @@ def sortByY(element, reverse=False):
 
 def turnRight(points, point2):
     '''
-        INPUT: 
+        INPUT:
             - point, list of two point2D, just the last two elements are used.
             - point2, Point2D instance, the last one
         RETURN: boolean, true if it represents a turn right.
