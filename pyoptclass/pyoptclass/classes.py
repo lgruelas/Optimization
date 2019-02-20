7f19add0a2021775bb8d339653cf4583d872b053
Germán Ruelas <lgruelas@hotmail.com>
2019-02-20 15:43:49 -0600

    Added cluster class
    remains the tests for that class

--- pyoptclass/pyoptclass/classes.py
+++ pyoptclass/pyoptclass/classes.py
@@ -1,3 +1,5 @@
+from pyoptclass import utils
+
 class Point2D:
     def __init__(self, x, y):
         self.X = float(x)
@@ -6,3 +8,34 @@ class Point2D:
         return "(%.2f, %.2f)" % (self.X, self.Y)
     def __eq__(self, point):
         return self.X == point.X and self.Y == point.Y
+
+class PdV(Point2D):
+    def __init__(self, x, y, time_store):
+        Point2D.__init__(self, x, y)
+        self.time_store = time_store
+    def __repr__(self):
+        return "[%s, %.2f]" % (Point2D.__repr__(self), self.time_store)
+    def samePlace(self, pdv):
+        return self == pdv
+
+class ClusterPdV:
+    def __init__(self, pdvs=[]):
+        self._elements = pdvs
+        self.total_time = 0
+        for i in pdvs:
+            self.total_time += i.time_store
+    def elements(self):
+        for i in self._elements:
+            yield i
+    def push_back(self, pdv):
+        self.total_time += pdv.time_store
+        self._elements.appned(pdv)
+    def pop_back(self):
+        self.total_time -= self._elements[-1].time_store
+        return self._elements.pop()
+    def convex_hull(self):
+        return utils.getConvexHull(self._elements)
+    def size(self):
+        return len(self._elements)
+    def area(self):
+        pass
--- pyoptclass/pyoptclass/tests/test_classes.py
+++ pyoptclass/pyoptclass/tests/test_classes.py
@@ -1,8 +1,8 @@
-import pyoptclass.classes as classes
+from pyoptclass import classes as cl
 
 def test_Point2D():
-    point_int = classes.Point2D(3,4)
-    point_float = classes.Point2D(3.5, 2.1)
+    point_int = cl.Point2D(3, 4)
+    point_float = cl.Point2D(3.5, 2.1)
     errors = []
     if not (point_int.X == 3 and point_int.Y == 4):
         errors.append('error')
@@ -11,4 +11,23 @@ def test_Point2D():
     if not (point_float.__repr__() == '(3.50, 2.10)' and point_int.__repr__() == '(3.00, 4.00)'):
         errors.append('error')
     assert not errors
-    
+
+def test_PdV():
+    point_int = cl.PdV(3, 4, 85.9)
+    point_float = cl.PdV(3.5, 2.1, 62.5)
+    point_float2 = cl.PdV(3.5, 2.1, 61.5)
+    errors = []
+    if not (point_int.X == 3 and point_int.Y == 4):
+        errors.append('error')
+    if not (point_float.X == 3.5 and point_float.Y == 2.1):
+        errors.append('error')
+    if not (point_float.__repr__() == '[(3.50, 2.10), 62.50]' and point_int.__repr__() == '[(3.00, 4.00), 85.90]'):
+        errors.append('error')
+    if not (point_float.samePlace(point_float2)):
+        errors.append('error')
+    if not (point_float.time_store == 62.5 and point_int.time_store == 85.9):
+        errors.append('error')
+    assert not errors
+
+def test_Cluster():
+    pass
