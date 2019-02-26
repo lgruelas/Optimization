22df2612cbdbfa7684449f21e59eda8e660d1d8b
GeorgeAlanis <georgesaaurio@gmail.com>
2019-02-25 20:57:27 -0600

    Default path to dataset fixed

--- pyoptclass/pyoptclass/utils.py
+++ pyoptclass/pyoptclass/utils.py
@@ -69,7 +69,7 @@ def getConvexPolygonArea(elements):
         area += elements[i+1].X * elements[i].Y
     return area/2.
 
-def getData(file_path='Sprint7ToroideMixto.csv'):
+def getData(file_path='../assets/Sprint7ToroideMixto.csv'):
     data = pd.read_csv(file_path)
     data['tiempo_en_tienda'] = data['demanda'] * data['frecuencia']
     stores = np.array(list(zip(data.lat.values, data.lon.values, data.tiempo_en_tienda.values)))
